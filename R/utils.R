f4c702436ecb1450813f36ee9de6c8bebb34edaa
Joe Futrelle <jfutrelle@whoi.edu>
2023-09-28 16:17:54 -0400

    url construction utilities

---
+++ R/utils.R
@@ -0,0 +1,22 @@
+
+construct_url_params <- function(params=NULL) {
+  param_strs <- sapply(names(params), function(key) {
+    if (!is.null(params[[key]])) {
+      paste0(key, '=', URLencode(params[[key]]))
+    } else {
+      NULL
+    }
+  })
+  param_strs <- param_strs[!sapply(param_strs, is.null)]
+  param_string <- paste(param_strs, collapse = '&')
+  if(param_string != "") {
+    return(paste0("?", param_string))
+  } else {
+    return("")
+  }
+}
+
+construct_url <- function(suffix, params=NULL) {
+  base_url <- Sys.getenv("NESLTER_API_URL")
+  return(paste0(base_url, suffix, construct_url_params(params)))
+}
